10a6ab75946d6201321dd6ded5d8cfef6ba02bc9
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-10-15 22:39:58 +0000

    feat(search): add toggle feature to search results
    - Add logic to render searchResults component when user search for songs.
    - Add toggle feature to close the rendered component(searchResults)
    upon user action on the delete button.
    - Enhance UI of the SearchResults component.

--- amber-tune/src/components/NavBar.jsx
+++ amber-tune/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="w-[100%] h-[10rem] bg-amber-100 flex justify-around items-center pt-6 ">
+    <nav className="w-[100%] absolute top-0 right-0 left-0 h-[10rem] bg-amber-100 z-10 flex justify-around items-center pt-6 ">
       <div className="w-[clamp(18rem,2.5vw,22rem)]">
         <img className="w-[100%] h-[100%] object-contain"  src="src/assets/amber-tune-logo.png" alt="AmberTune Logo" />
       </div>
--- amber-tune/src/components/Search.jsx
+++ amber-tune/src/components/Search.jsx
@@ -5,37 +5,36 @@ import { useApiStore } from "../store/GlobalApiStore";
 
 function MusicSearch() {
   const setMusicData = useApiStore((state) => state.setMusicData);
+  const setIsVisible = useApiStore((state) => state.setIsVisible);
 
   const [searchTerm, setSearchTerm] = useState("");
 
-  const {  isLoading, isError, error, refetch } = useQuery({
+  const { isLoading, isError, error, refetch } = useQuery({
     queryKey: ["MusicData", searchTerm],
     queryFn: () => fetchMusicData(searchTerm),
     enabled: false,
-  
+    refetchOnWindowFocus: false,
   });
 
   const handleSearch = async (e) => {
     e.preventDefault();
     if (!searchTerm.trim()) return;
-  let refetchData = await refetch();
-    if(refetchData.data) {
-        setMusicData(refetchData.data)
+    setIsVisible(false);
+    const refetchData = await refetch();
+    if (refetchData.data) {
+      setMusicData(refetchData.data);
+      setIsVisible(true);
     }
-   
   };
 
   if (isLoading) return <p>Loading...</p>;
   if (isError) return <p>Something went wrong: {error.message}</p>;
   return (
-    <div className="w-[100%] bg-gray-500 flex justify-center">
-      <div className="w-[35rem] h-[5rem] border flex items-center justify-center bg-amber-300">
-        <form
-          onSubmit={handleSearch}
-          className="w-full min-w-[2rem  bg-amber-200"
-        >
+    <div className="w-[100%] flex justify-center ">
+      <div className="w-[35rem] h-[5rem] flex items-center justify-center  ">
+        <form onSubmit={handleSearch} className="w-full min-w-[2rem] mt-5">
           {/* form field */}
-          <div className="border bg-red-500 flex items-center justify-center min-w-[2rem] relative">
+          <div className=" flex items-center justify-center min-w-[2rem] relative">
             {/* search icon */}
             <div className="absolute left-[1.1rem] top-[0.7rem] cursor-pointer">
               <svg
@@ -60,12 +59,12 @@ function MusicSearch() {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               placeholder="What do you want to play?"
-              className="px-[5%] pl-[4rem]  border  w-full min-w-[5rem] h-[3.8rem]   outline-none bg-amber-100 rounded-3xl rounded-r-none placeholder-amber-900 text-[clamp(1.2rem,1.5vw,1.6rem)]"
+              className="px-[5%] pl-[4rem]  border border-r-0  w-full min-w-[5rem] h-[3.8rem]   outline-none bg-amber-100 rounded-3xl rounded-r-none placeholder-amber-900 text-[clamp(1.2rem,1.5vw,1.6rem)]"
             />
 
             {/* search button */}
             <button
-              className="border h-[3.8rem] px-[1.6rem] rounded-3xl rounded-l-none bg-amber-100  text-amber-900"
+              className="border border-amber-900 border-l-0 h-[3.8rem] px-[1.6rem] rounded-3xl rounded-l-none cursor-pointer bg-amber-200  text-amber-900"
               type="submit"
             >
               Search
