50028fe9ca277a994612c0b1062a80144f17b0f7
Samuel Akongbota <122745519+Reysam2@users.noreply.github.com>
2025-10-19 23:12:22 +0000

    fix: broken music app logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
+import image from "../assets/amber-tune-logo.png"
+
 function NavBar() {
   return (
     <nav className="w-[100%] absolute top-0 right-0 left-0 h-[10rem] bg-amber-100 z-10 flex justify-around items-center pt-6 ">
       <div className="w-[clamp(18rem,2.5vw,22rem)]">
-        <img className="w-[100%] h-[100%] object-contain"  src="src/assets/amber-tune-logo.png" alt="AmberTune Logo" />
+        <img className="w-[100%] h-[100%] object-contain"  src={image} alt="AmberTune Logo" />
       </div>
 
       <div className="w-[55%] h-[80%]  flex justify-center items-center gap-x-10 text-amber-900">
